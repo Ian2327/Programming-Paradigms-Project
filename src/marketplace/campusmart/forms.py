7cf3a6ee689e05ccd60c3056fe158d0a4974540e
Wesley Omerso <womerso@student05.cse.nd.edu>
2025-04-12 18:22:39 -0400

    Added more navbar control + basic bootstrap

--- src/marketplace/campusmart/forms.py
+++ src/marketplace/campusmart/forms.py
@@ -22,11 +22,11 @@ class CreateListingForm(forms.Form):
         ("F" , "Fair"),
         ("P" , "Poor")
     ]
-    title = forms.CharField(max_length=200)
-    description = forms.CharField(max_length=4000, widget=forms.Textarea)
-    price = forms.DecimalField(decimal_places=2, min_value=0.01)
-    condition = forms.ChoiceField(choices=conditionChoices)
-    image = forms.ImageField()
+    title = forms.CharField(max_length=200, widget=forms.TextInput(attrs={'class':'form-control'}))
+    description = forms.CharField(max_length=4000, widget=forms.Textarea(attrs={'class':'form-control'}))
+    price = forms.DecimalField(decimal_places=2, min_value=0.01, widget=forms.NumberInput(attrs={'class':'form-control'})) 
+    condition = forms.ChoiceField(choices=conditionChoices, widget=forms.Select(attrs={'class':'form-control'}))
+    image = forms.ImageField(widget=forms.FileInput(attrs={'class':'form-control'}))
 
 ListingImageForm = forms.inlineformset_factory(
     parent_model=Listing,
